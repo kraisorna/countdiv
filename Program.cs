83aad1ec99c710ba8bf46c32faa46c1abcef3660
Kraisorn Arjharn <kraisorna@aol.com>
2017-03-13 17:47:02 +0700

    First local commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountDiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.solution(11,345,17));
+            Console.ReadLine();
+        }
+    }
+
+    class Solution {
+        public int solution(int A, int B, int K) {
+            int num = 0;
+            num = (B / K) - (A / K);
+            if ((A % K)==0)
+                num++;
+            return num;
+        }
+    }
+}
